9b32a9d22818b9ca2edd0e7a413bed022b1fbb78
gowllaanilkumar <anilkumar.gakr@gmail.com>
2023-12-30 15:09:36 +0530

    shell script -2

--- 02.varibles.sh
+++ 02.varibles.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 PERSON1=$1
-PERSON2=Ramesh$2
+PERSON2=$2
 
 echo "$PERSON1: Hello $PERSON2 Good morning"
 echo "$PERSON2: Hi $PERSON1 good morning how do you do"
 echo "$PERSON1: Iam doing good"
 echo "$PERSON2: Thank you"
 echo "$PERSON1:  Welcome"
+
+USER=$1
+PASSWORD=$2
+echo "user is : $USER, password is : $PASSWORD"
