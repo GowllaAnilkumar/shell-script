34209e5f3991332ff4556fd0228214ff51e2bdc6
gowllaanilkumar <anilkumar.gakr@gmail.com>
2023-12-30 20:43:06 +0530

    shell script -2

--- 04.arrays.sh
+++ 04.arrays.sh
@@ -3,7 +3,6 @@ FRUITS=("apple" "mango" "promo")
 echo "first value: ${FRUITS[0]}"
 echo "second value: ${FRUITS[1]}"
 echo "second value: ${FRUITS[2]}"
-
 echo "All fruits: ${FRUITS[@]}"
 # varible is nothing but storage
 
---
+++ 05.conditions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+NUMBER=$
+if "[$NUMBER -gt 100]"
+then
+   echo "given $NUMBER is graterthan 100"
+else
+    echo "given $NUMBER is not gratherthan 100"
+fi
