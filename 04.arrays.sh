442dd3afc7230d87792cd332856e32e63b3fe630
gowllaanilkumar <anilkumar.gakr@gmail.com>
2023-12-30 20:17:46 +0530

    shell script -2

--- 04.arrays.sh
+++ 04.arrays.sh
@@ -3,3 +3,7 @@ FRUITS=("apple" "mango" "promo")
 echo "first value: ${FRUITS[0]}"
 echo "second value: ${FRUITS[1]}"
 echo "second value: ${FRUITS[2]}"
+
+echo "all fruits: $(FRUITS[@])"
+# varible is nothing but storage
+
