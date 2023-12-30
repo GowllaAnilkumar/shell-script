1d11a6ff6964f178ce91fc57a7a0233ab55ea033
gowllaanilkumar <anilkumar.gakr@gmail.com>
2023-12-30 21:08:19 +0530

    shell script -2

--- 05.conditions.sh
+++ 05.conditions.sh
@@ -2,7 +2,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 100 ] 
 then
-   echo "given number $NUMBER is graterthan 100"
+   echo "given number $NUMBER is grater than 100"
 else
-    echo "given number $NUMBER is not gratherthan 100"
+    echo "given number $NUMBER is not grather than 100"
 fi
