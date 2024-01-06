45b9830e0367b66f65e32fe73fa696bccb1db92d
gowllaanilkumar <anilkumar.gakr@gmail.com>
2024-01-06 18:09:36 +0530

    shell script -2

---
+++ 08.logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ID=$(id -u)
+echo "script name $0 "
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then   
+        echo "ERROR: : $1 ... faild"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: : please run this script with root acess"
+else
+    echo "you are root user"
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+yum install git -y
+VALIDATE $? "installing git"
