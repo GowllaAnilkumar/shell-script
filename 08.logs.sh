95c8107eac3ce4b0295d73e56cc356910ef44a82
gowllaanilkumar <anilkumar.gakr@gmail.com>
2024-01-06 19:57:57 +0530

    shell script -2

--- 08.logs.sh
+++ 08.logs.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="/e[31m"
+G="/e[32m"
+N="/e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script name $0"
 VALIDATE(){
     if [ $? -ne 0 ]
     then   
-        echo "ERROR: : $1 ... faild"
+        echo -e "ERROR: : $1 ... $R faild $N"
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 if [ $ID -ne 0 ]
 then
-    echo "ERROR: : please run this script with root acess"
+    echo "$R ERROR: : please run this script with root acess $N"
 else
     echo "you are root user"
 fi
